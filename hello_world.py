6aac59dd5f3cba95d8b2fb89674b3c5a195c431c
seanmckinley <sean.mckinley@outlook.com>
2013-05-19 01:18:56 -0600

    removing fibbonacci sequence from our hello world

--- hello_world.py
+++ hello_world.py
@@ -9,14 +9,3 @@ directory=["headquarters", "hotel", "hosptial", "house", "hotdog"]
 
 for building in directory:
 	print building
-
-fib=[1,2]
-
-def function_fib():
-	while len(fib) < 100:
-		last=fib[-1]
-		second_to_last=fib[-2]
-		fib.append(last+second_to_last)
-	return fib
-
-print function_fib()
