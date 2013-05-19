9a5aa111a6455daec039eeea6e435e25a4db2bd5
terrance.pendergrass@gmail.com <terrance.pendergrass@gmail.com>
2013-05-19 01:31:44 -0500

    corrected values in array

--- hello_world.py
+++ hello_world.py
@@ -3,8 +3,7 @@
 print "Hello World, Terrance."
 print "Change made by"
 
-directory=[headquarters, hotel, hosptial, house, hotdog]
+directory=["headquarters", "hotel", "hosptial", "house", "hotdog"]
 
 for building in directory:
 	print building
-
