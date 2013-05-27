9a76424ea4177575619779da3a3259c51f345541
seanmckinley <sean.mckinley@outlook.com>
2013-05-27 01:30:32 -0600

    i am wrong

--- planner.py
+++ planner.py
@@ -1,6 +1,25 @@
-def main ():
-	print "Welcome"
-	print "1 - Add Event"
-	print "2 - Search for Event"
-	print "3 - Remove Event"
-	print "4 - Exit Program"
+# def main ():
+# 	print "Welcome"
+# 	print "1 - Add Event"
+# 	print "2 - Search for Event"
+# 	print "3 - Remove Event"
+# 	print "4 - Exit Program"
+
+while True:
+	# print "Welcome"
+	# print "1 - Add Event"
+	# print "2 - Search for Event"
+	# print "3 - Remove Event"
+	# print "4 - Exit Program"
+
+  placeholdervariable = raw_input("Enter 1-4 or fuck off")
+
+	if placeholdervariable == 1:
+		sys.exit()
+	elif placeholdervariable == 2:
+		funcplaceholder()
+	elif placeholdervariable == 3:
+		funcanotherplaceholderfuck()
+	else:
+		print "Please enter a valid option."
+		continue
