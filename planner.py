a0a135bbccca553fbadad6ad44fdd6944ee9da84
seanmckinley <sean.mckinley@outlook.com>
2013-05-27 02:07:28 -0600

    adding saving of schedule via writing out an unencrypted, completely plain text, open to the world file

--- planner.py
+++ planner.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import sys
+import os.path
+import os
+
 # def main ():
 # 	print "Welcome"
 # 	print "1 - Add Event"
@@ -9,20 +13,31 @@
 planner={}
 
 def addEvent(planner):
-	time = raw_input ("Please select a time for this event")
-	event = raw_input ("Select event to occur at this time")
+	time = raw_input ("Please select a time for this event: ")
+	event = raw_input ("Select event to occur at this time: ")
 	planner[time]= event
 	return planner
 
+def write():
+	if os.path.exists('plans.txt') == True:
+		varWriteOut = open('plans.txt', "a")
+		varWriteOut.write('%s\n' % (planner))
+		varWriteOut.close()
+	else:
+		varWriteOut = open('plans.txt', "w")
+		varWriteOut.write('%s\n' % (planner))
+		varWriteOut.close()
+
 
 while True:
-	placeholdervariable = raw_input("Enter 1-4 or fuck off")
+	placeholdervariable = raw_input("Enter 1-4 or fuck off: ")
 
 	if placeholdervariable == "1":
 		sys.exit()
 	elif placeholdervariable == "2":
 		addEvent(planner)
 		print planner
+		write()
 	elif placeholdervariable == "3":
 		funcanotherplaceholderfuck()
 	else:
