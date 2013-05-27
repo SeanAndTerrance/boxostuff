7ab7ca884b5374b9486abdc451b8fccd2efc61be
Terrance P <terrance.pendergrass@gmail.com>
2013-05-27 02:58:47 -0500

    Created add function

--- planner.py
+++ planner.py
@@ -6,16 +6,26 @@
 # 	print "2 - Search for Event"
 # 	print "3 - Remove Event"
 # 	print "4 - Exit Program"
+planner={}
+
+def addEvent(planner):
+	time = raw_input ("Please select a time for this event")
+	event = raw_input ("Select event to occur at this time")
+	planner[time]= event
+	return planner
+
 
 while True:
 	placeholdervariable = raw_input("Enter 1-4 or fuck off")
 
-	if placeholdervariable == 1:
+	if placeholdervariable == "1":
 		sys.exit()
-	elif placeholdervariable == 2:
-		funcplaceholder()
-	elif placeholdervariable == 3:
+	elif placeholdervariable == "2":
+		addEvent(planner)
+		print planner
+	elif placeholdervariable == "3":
 		funcanotherplaceholderfuck()
 	else:
 		print "Please enter a valid option."
 		continue
+
